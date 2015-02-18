8192c63907de422a4e802d122b38c9f2336d9d40
Nakul Pathak <nakulpathak3@hotmail.com>
2015-02-17 23:06:02 -0500

    Seed db with 2 books and list books with rating filter

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,13 @@ class BooksController < ApplicationController
 
   def index
     books = Book.all
+
+    if rating = params[:rating]
+      books = books.where(rating: rating)
+    end
+
     render json: books, status: 200
+
   end
 
 end
